793884b51890b493dc6503c2ba83e37e2c094de3
SafiaEzzahir <safia.ezzahir@hotmail.com>
2026-01-06 22:49:52 +0000

    Add CurrentPage state machine to choose what page to go onto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
+import { useState } from 'react'
 import './App.css'
 // ^ that's how you import css files
 
 function App() {
+  const [CurrentPage, setCurrentPage] = useState("home")
+  /* see EXPLANATION for what useState is, basically CurrentPage is a variable,
+  and setCurrentPage can change it, leading to a render (so the page changes! - if needed)
+  
+  when you first do useState() the thing inside the brackets is what you set the
+  variable to at the start, in this case: "home"
+
+  to change CurrentPage: just do setCurrentPage(what you want to set it as)*/
+
   // this is the main app, don't put all of your code straight in here - make other files
-  return (
-    <div>
-      <h1>hello this is safia's header</h1>
-      <p>hiiiii i am cow</p>
-    </div>
-  )
+
+  if (CurrentPage == "home"){
+    return (
+      <div>
+        <h1>hello this is safia's header</h1>
+        <p>hiiiii i am cow</p>
+        <button onClick={setCurrentPage("bigcow")}>click me!</button>
+      </div>
+    )
+  } else if (CurrentPage == "bigcow") {
+    return (
+      <BigCowPage />
+    )
+  } else if (CurrentPage == "babycow") {
+    return (
+      <BabyCowPage />
+    )
+  } else if (CurrentPage == "specialcow") {
+    return (
+      <SpecialCowPage />
+    )
+  }
+
 }
 
 export default App
